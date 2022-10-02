a343dbab5a45a8b7d48c50aa8cf69edd27ab6f94
Zmarzlik <101973074+Zmarzlik@users.noreply.github.com>
2022-10-02 18:01:42 +0200

    Task 1, 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientsList = document.querySelector("#ingredients");
+const newList = ingredients.map((ingredient) => {
+  const items = document.createElement("li");
+  items.textContent = ingredient;
+  items.classList.add("item");
+  return items;
+});
+
+ingredientsList.append(...newList);
+
+console.log(ingredientsList);
+console.log(ingredients);
