f9d26b4426d0253c9c12713bb2d1df451d72f59a
Zmarzlik <101973074+Zmarzlik@users.noreply.github.com>
2022-10-02 19:59:00 +0200

    Add tasks 3-6

---
+++ js/task-04.js
@@ -0,0 +1,19 @@
+
+const decerementButton = document.querySelector('[data-action="decrement"]');
+const incrementButton = document.querySelector('[data-action="increment"]');
+const counter = document.querySelector("#value");
+
+let counterValue = 0;
+
+const decrement = () => {
+  counter.textContent = counterValue -= 1;
+};
+
+const increment = () => {
+  counter.textContent = counterValue += 1;
+};
+
+decerementButton.addEventListener("click", decrement);
+incrementButton.addEventListener("click", increment);
+
+
